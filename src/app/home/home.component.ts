d50b91c9610a3089f6382852acc3123304426eb7
electroluxv2 <electroluxv2@gmail.com>
2022-01-04 17:53:53 +0100

    Basic styling

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -5,7 +5,7 @@ import { map, Observable } from 'rxjs';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
   posts: Observable<ScullyRoute[]> = this.scully.available$.pipe(map(routes => routes.filter(route => route.route.startsWith('/blog/') && route.sourceFile?.endsWith('.md'))));
