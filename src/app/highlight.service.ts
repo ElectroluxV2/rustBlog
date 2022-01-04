dd9d6d88a11fe809bfb63a3291ab34babccbfc4d
electroluxv2 <electroluxv2@gmail.com>
2022-01-04 20:23:57 +0100

    Better code highlight

--- src/app/highlight.service.ts
+++ src/app/highlight.service.ts
@@ -1,13 +1,8 @@
 import { Inject, Injectable, PLATFORM_ID } from '@angular/core';
 import { isPlatformBrowser } from "@angular/common";
-import * as Prism from 'prismjs';
-import 'prismjs/components/prism-bash';
-import 'prismjs/components/prism-css';
-import 'prismjs/components/prism-javascript';
-import 'prismjs/components/prism-json';
-import 'prismjs/components/prism-markup';
-import 'prismjs/components/prism-typescript';
-import 'prismjs/plugins/copy-to-clipboard/prism-copy-to-clipboard';
+import hljs from 'highlight.js';
+
+
 
 @Injectable({
   providedIn: 'root'
@@ -18,7 +13,7 @@ export class HighlightService {
 
   public highlightAll(): void {
     if (isPlatformBrowser(this.platformId)) {
-      Prism.highlightAll();
+      hljs.highlightAll();
     }
   }
 }
